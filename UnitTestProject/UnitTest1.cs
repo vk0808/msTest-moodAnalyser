8189a5e797ddf267dbd78d8882065d7ff2a0560a
vk0808 <vk4765712@gmail.com>
2021-08-23 16:31:57 +0530

    test: case 1.2 happy

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -10,6 +10,21 @@ namespace UnitTestProject
         public void WhenGivenMoodSad_ShouldReturnSad()
         {
             //Arrange
+            string expected = "SAD";
+            string message = "I am in Sad Mood";
+            MoodAnalyser moodAnalyzer = new MoodAnalyser(message);
+
+            //Act
+            string mood = moodAnalyzer.AnalyseMood();
+
+            //Assert
+            Assert.AreEqual(expected, mood);
+        }
+
+        [TestMethod]
+        public void WhenGivenMoodHappy_ShouldReturnHappy()
+        {
+            //Arrange
             string expected = "HAPPY";
             string message = "I am in Happy Mood";
             MoodAnalyser moodAnalyzer = new MoodAnalyser(message);
