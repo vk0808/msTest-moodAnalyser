9b8226e9b1b0b491c7252f387ba7c316d728c38f
vk0808 <vk4765712@gmail.com>
2021-08-24 02:14:41 +0530

    feat: handle parameterised ctor

--- AnalyseMood/MoodAnalyserFactory.cs
+++ AnalyseMood/MoodAnalyserFactory.cs
@@ -19,7 +19,7 @@ namespace AnalyseMood
             this.message = message;
         }
 
-        // method to handle exception if there is no class or contructor
+        // method to handle exception if there is no class or default contructor
         public static object CreateMoodAnalyse(string className, string constructorName)
         {
             string pattern = @"." + constructorName + "$";
@@ -41,6 +41,30 @@ namespace AnalyseMood
             else
             {
                 throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+            }
+        }
+
+        // method to handle exception if there is no class or parameterised contructor
+        public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+        {
+            Type type = typeof(MoodAnalyser);
+            if (type.Name.Equals(className) || type.FullName.Equals(className))
+            {
+                if (type.Name.Equals(constructorName))
+                {
+                    ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                    object instance = ctor.Invoke(new object[] { message });
+                    return instance;
+                }
+                else
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
             }
         }
     }
