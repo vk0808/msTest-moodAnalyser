6d52e6f23987ed29f96591c63b30cf8ffb2dce3f
vk0808 <vk4765712@gmail.com>
2021-08-24 01:19:29 +0530

    feat: create reflection for default ctor

---
+++ AnalyseMood/MoodAnalyserFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AnalyseMood
+{
+    public class MoodAnalyserFactory
+    {
+        // instance variable
+        private string message;
+
+        // constructor
+        public MoodAnalyserFactory(string message)
+        {
+            this.message = message;
+        }
+
+        // method to handle exception if there is no class or contructor
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string pattern = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattern);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+                }
+                catch (ArgumentNullException)
+                {
+                    throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
+                }
+            }
+            else
+            {
+                throw new MoodAnalyserCustomException(MoodAnalyserCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+            }
+        }
+    }
+}
